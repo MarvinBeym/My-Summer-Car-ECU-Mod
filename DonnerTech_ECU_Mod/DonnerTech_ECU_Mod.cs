6ed512e4474521e4e795410565cd8670e327cac5
Marvin Beym <Marvin.Beym@gmx.de>
2021-10-31 08:08:10 +0100

    Increase version

--- DonnerTech_ECU_Mod/DonnerTech_ECU_Mod.cs
+++ DonnerTech_ECU_Mod/DonnerTech_ECU_Mod.cs
@@ -88,7 +88,7 @@ namespace DonnerTech_ECU_Mod
 		public override string ID => "DonnerTech_ECU_Mod"; //Your mod ID (unique)
 		public override string Name => "DonnerTechRacing ECUs"; //You mod name
 		public override string Author => "DonnerPlays"; //Your Username
-		public override string Version => "1.5.6"; //Version
+		public override string Version => "1.5.7"; //Version
 		public override bool UseAssetsFolder => true;
 		
 		public AssetBundle assetBundle;
