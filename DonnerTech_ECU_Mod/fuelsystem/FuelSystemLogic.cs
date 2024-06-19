073d63195785f7945c40ecaf0bf8bfedd6f75394
Marvin Beym <Marvin.Beym@gmx.de>
2024-06-19 19:38:17 +0200

    Improve method

--- DonnerTech_ECU_Mod/fuelsystem/FuelSystemLogic.cs
+++ DonnerTech_ECU_Mod/fuelsystem/FuelSystemLogic.cs
@@ -16,6 +16,8 @@ namespace DonnerTech_ECU_Mod
 {
 	public class FuelSystemLogic : MonoBehaviour
 	{
+		protected const int RPM_STEP = 500;
+		protected const int RPM_INDEX_LIMIT = 16;
 		private DonnerTech_ECU_Mod mod;
 		private FuelSystem fuel_system;
 		private Transform[] throttleBodyValves;
@@ -100,43 +102,14 @@ namespace DonnerTech_ECU_Mod
 
 		private int GetRPMIndex(int rpm)
 		{
-			switch (rpm)
+			int index = Mathf.FloorToInt(rpm / RPM_STEP) - 1;
+
+			if (index >= RPM_INDEX_LIMIT)
 			{
-				case int r when r <= 500:
-					return 0;
-				case int r when r <= 1000:
-					return 1;
-				case int r when r <= 1500:
-					return 2;
-				case int r when r <= 2000:
-					return 3;
-				case int r when r <= 2500:
-					return 4;
-				case int r when r <= 3000:
-					return 5;
-				case int r when r <= 3500:
-					return 6;
-				case int r when r <= 4000:
-					return 7;
-				case int r when r <= 4500:
-					return 8;
-				case int r when r <= 5000:
-					return 9;
-				case int r when r <= 5500:
-					return 10;
-				case int r when r <= 6000:
-					return 11;
-				case int r when r <= 6500:
-					return 12;
-				case int r when r <= 7000:
-					return 13;
-				case int r when r <= 7500:
-					return 14;
-				case int r when r <= 8000:
-					return 15;
-				default:
-					return 16;
+				index = RPM_INDEX_LIMIT;
 			}
+
+			return index;
 		}
 
 		public void Init(FuelSystem fuel_system, DonnerTech_ECU_Mod mod)
