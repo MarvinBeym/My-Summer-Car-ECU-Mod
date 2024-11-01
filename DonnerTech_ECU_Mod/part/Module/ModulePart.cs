8c8394efc63a398d6cde915434510c00a7edd70c
Marvin Beym <Marvin.Beym@gmx.de>
2024-11-01 12:30:50 +0100

    Add event listeners to disable ModulePart on uninstall/unbolt

--- DonnerTech_ECU_Mod/part/Module/ModulePart.cs
+++ DonnerTech_ECU_Mod/part/Module/ModulePart.cs
@@ -1,4 +1,5 @@
 ﻿using MscModApi.Parts;
+using MscModApi.Parts.EventSystem;
 using MscModApi.Parts.ReplacePart;
 using UnityEngine;
 
@@ -11,10 +12,24 @@ namespace DonnerTech_ECU_Mod.part.Module
 
 		protected ModulePart(DerivablePart parent) : base(parent, DonnerTech_ECU_Mod.partBaseInfo)
 		{
+			SetupEventListeners();
 		}
 
 		protected ModulePart(GamePart parent) : base(parent, DonnerTech_ECU_Mod.partBaseInfo)
 		{
+			SetupEventListeners();
+		}
+
+		private void SetupEventListeners()
+		{
+			AddEventListener(PartEvent.Time.Post, PartEvent.Type.Uninstall, Disable);
+			AddEventListener(PartEvent.Time.Post, PartEvent.Type.Unbolted, Disable);
+			AddEventListener(PartEvent.Time.Post, PartEvent.Type.UninstallFromCar, Disable);
+		}
+
+		protected void Disable()
+		{
+			enabled = false;
 		}
 
 		public void Toggle()
